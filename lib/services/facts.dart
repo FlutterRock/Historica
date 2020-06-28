e3a3013a14b3674e3be00147dac0df884c048a73
Prastab <67535533+Prastab@users.noreply.github.com>
2020-06-28 14:57:43 +0545

    Add files via upload

---
+++ lib/services/facts.dart
@@ -0,0 +1,13 @@
+import 'networking.dart';
+
+const url = 'http://history.muffinlabs.com/date';
+
+class FactModel {
+  static Future<dynamic> getFact(int month, int date) async {
+    NetworkHelper networkHelper = NetworkHelper('$url/$month/$date');
+
+    var factRawData = await networkHelper.getData();
+    print(factRawData);
+    return (factRawData);
+  }
+}
