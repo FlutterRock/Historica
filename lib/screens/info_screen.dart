a3ab0b0422a70767aa9a8ffd73c2dbfccb96423f
Prastab <prastabg16@gmail.com>
2020-06-29 11:43:32 +0545

    gesture detector

--- lib/screens/info_screen.dart
+++ lib/screens/info_screen.dart
@@ -14,7 +14,6 @@ class InfoScreen extends StatefulWidget {
 }
 
 class _InfoScreenState extends State<InfoScreen> {
-
   int index;
 
   @override
@@ -28,23 +27,22 @@ class _InfoScreenState extends State<InfoScreen> {
     int limit = widget.data.data.events.length;
     return Scaffold(
       floatingActionButton: OutlineButton.icon(
-          onPressed: (){
+          onPressed: () {
             setState(() {
-              if(index == limit-1) {
-              }
-              else {
+              if (index == limit - 1) {
+              } else {
                 index++;
               }
             });
           },
           icon: Icon(Icons.navigate_next),
           label: Text('Next Fact')),
-      body:  Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('images/11.jpg'),
-              fit: BoxFit.fill,
-            ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/11.jpg'),
+            fit: BoxFit.fill,
+          ),
 //          gradient: RadialGradient(
 //            radius: 1.5,
 //            stops: [
@@ -56,25 +54,25 @@ class _InfoScreenState extends State<InfoScreen> {
 //              Color(0xFF64ffda),
 //            ],
 //          ),
-          ),
-          constraints: BoxConstraints.expand(),
-          child: SingleChildScrollView(
-            child: SafeArea(
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(
-                  18.0,
-                  30.0,
-                  18.0,
-                  18.0,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    DateShow(
-                      widget.aDay,
-                    ),
-                    /*Column(
+        ),
+        constraints: BoxConstraints.expand(),
+        child: SingleChildScrollView(
+          child: SafeArea(
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(
+                18.0,
+                30.0,
+                18.0,
+                18.0,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  DateShow(
+                    widget.aDay,
+                  ),
+                  /*Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
@@ -83,44 +81,48 @@ class _InfoScreenState extends State<InfoScreen> {
                         ),
                       ],
                     ),*/
-                    SizedBox(
-                      height: 20.0,
-                      width: double.infinity,
-                      child: Divider(
-                        color: Colors.red,
-                      ),
+                  SizedBox(
+                    height: 20.0,
+                    width: double.infinity,
+                    child: Divider(
+                      color: Colors.red,
                     ),
-                    Text(
-                      'Today in',
-                      style: kHeaderTextStyle,
+                  ),
+                  Text(
+                    'Today in',
+                    style: kHeaderTextStyle,
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text(
+                    'History',
+                    style: kHeaderTextStyle,
+                  ),
+                  SizedBox(
+                    height: 40.0,
+                    width: double.infinity,
+                    child: Divider(
+                      indent: 20.0,
+                      endIndent: 400.0,
                     ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Text(
-                      'History',
-                      style: kHeaderTextStyle,
-                    ),
-                    SizedBox(
-                      height: 40.0,
-                      width: double.infinity,
-                      child: Divider(
-                        indent: 20.0,
-                        endIndent: 400.0,
-                      ),
-                    ),
-                    Text(
+                  ),
+                  GestureDetector(
+                    onLongPress: () {},
+                    child: Text(
                       'In year ${widget.data.data.events[index].year}, ${widget.data.data.events[index].text}',
                       style: kDateTextStyle1,
                     ),
-                  ],
-                ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                ],
               ),
             ),
           ),
+        ),
       ),
     );
   }
 }
-
-
